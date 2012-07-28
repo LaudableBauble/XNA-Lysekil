52366cca702f5f624c5e411e89335794e079dd57
christian.wigren <christian.wigren@valtech.se>
2012-07-29 01:57:49 +0200

    using Rectanglein figure

--- Tetris/Tetris/Tetris/Figure.cs
+++ Tetris/Tetris/Tetris/Figure.cs
@@ -1,16 +1,18 @@
-﻿namespace Tetris
+﻿using Microsoft.Xna.Framework;
+
+namespace Tetris
 {
-    abstract class Figure
+    class Figure
     {
         protected bool[,] Grid;
 
-        public int Xpos { get; set; }
-        public int Ypos { get; set; }
+        public Rectangle Rectangle;
         public int Rotate { get; set; }
 
-        protected Figure()
+        public Figure()
         {
             Grid = new bool[4, 4];
+            Rectangle = new Rectangle(0, 0, 32, 32);
         }
     }
 
@@ -28,8 +30,87 @@
         }
     }
 
-    class Hook : Figure
+    class Straight : Figure
     {
+        public Straight()
+        {
+            Grid = new bool[4, 4]
+            {
+                { true, false, false, false },
+                { true, false, false, false },
+                { true, false, false, false },
+                { true, false, false, false }
+            };
+        }
+    }
 
+    class HookRight : Figure
+    {
+        public HookRight()
+        {
+            Grid = new bool[4, 4]
+            {
+                { false, true, false, false },
+                { false, true, false, false },
+                { false, true, false, false },
+                { false, true, true,  false }
+            };
+        }
+    }
+
+    class HookLeft : Figure
+    {
+        public HookLeft()
+        {
+            Grid = new bool[4, 4]
+            {
+                { false, false, true, false },
+                { false, false, true, false },
+                { false, false, true, false },
+                { false,  true, true,  false }
+            };
+        }
+    }
+
+    class TwixRight : Figure
+    {
+        public TwixRight()
+        {
+            Grid = new bool[4, 4]
+            {
+                { false, false, true, false },
+                { false, false, true, false },
+                { false, true, false, false },
+                { false,  true, true,  false }
+            };
+        }
+    }
+
+    class Twixleft : Figure
+    {
+        public Twixleft()
+        {
+            Grid = new bool[4, 4]
+            {
+                { false,  true, false, false },
+                { false,  true, false, false },
+                { false, false,  true, false },
+                { false,  true,  true, false }
+            };
+        }
+    }
+
+    class Arrow : Figure
+    {
+        public Arrow()
+        {
+            Grid = new bool[4, 4]
+            {
+                { true,  true,  true,  false },
+                { false, true,  false, false },
+                { false, false, false, false },
+                { false, false, false, false }
+            };
+        }
     }
 }
